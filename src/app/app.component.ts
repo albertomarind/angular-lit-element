c41ee946527bcd19885997304293d89a2f2a6fd2
Alberto Marin <alberto.marind@gmail.com>
2021-01-12 21:18:19 -0600

    se implementa elemento de lit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-lit-element';
+  public items: string;
+  public selectedOption = 0;
+  public menuData = null;
+  constructor() {
+    let data = [
+      {
+        urlImg: '../assets/images/image.png',
+        urlImgActive: '../assets/images/image-active.png',
+        label: 'Opción 1',
+        selected: true,
+      },
+      {
+        urlImg: '../assets/images/image.png',
+        urlImgActive: '../assets/images/image-active.png',
+        label: 'Opción 2',
+        selected: false,
+      },
+      {
+        urlImg: '../assets/images/image.png',
+        urlImgActive: '../assets/images/image-active.png',
+        label: 'Opción 3',
+        selected: false,
+      },
+      {
+        urlImg: '../assets/images/image.png',
+        urlImgActive: '../assets/images/image-active.png',
+        label: 'Opción 4',
+        selected: false,
+      },
+      {
+        urlImg: '../assets/images/image.png',
+        urlImgActive: '../assets/images/image-active.png',
+        label: 'Opción 5',
+        selected: false,
+      },
+    ];
+    this.items = JSON.stringify(data);
+  }
+  onSelectedItem({detail}) {
+    this.selectedOption = detail.index;
+    console.log(detail);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import 'navigation-bar/navigation-bar.js';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ], 
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
